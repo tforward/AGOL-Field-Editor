96d42ffe5f4b06e0f9bf20871b0692557f6ddc9f
Tristan Forward <tforward@users.noreply.github.com>
2018-01-12 22:15:23 -0500

    OOLO rework

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// This JS file uses the OOLO Design Pattern
+
 const myApp = {};
 
 // ======================================================================
@@ -14,15 +16,13 @@ myApp.main = function main(){
   myApp.field_objects = get_unique_field_objs(json_data);
   myApp.field_names = Object.keys(myApp.field_objects);
 
-  widget_shell();
+  WidgetShell();
 
-  const fields = add_fields("content");
+  let fields = add_fields("content");
 
-  //console.log(fields)
-  
   add_btns(fields);
 
-  test()
+  //console.log(fields)
 }
 
 // ======================================================================
@@ -33,7 +33,6 @@ function parse_json(data){
 	const text_data = document.getElementById(data).value
 	return JSON.parse(text_data)
 }
-
 
 function get_unique_field_objs(json_data) {
     const field_names_set = new Set();
@@ -56,11 +55,12 @@ function get_unique_field_objs(json_data) {
 // Add Elements
 // ======================================================================
 
-function widget_shell(){
+function WidgetShell(){
+    // Test this with the other style to see how it works again let widget
     myApp.Widget = {
-      init: function(){
-        this.id = null;
-        this.elem = null;
+      init: function(id, elem){
+        this.id = id;
+        this.elem = elem;
         return this
       },
       addTo: function(parent){
@@ -70,14 +70,13 @@ function widget_shell(){
   }
 
 
-function field_factory(){
+function FieldDelegator(){
     const field = Object.create(myApp.Widget)
 
     field.setup = function(id){
-        this.init();
-        this.id = id;
-        this.elem = document.createElement("div");
+        this.init(id, document.createElement("div"));
         this.elem.className = "aligner-div";
+        return this
     },
     field.define = function(){
         this.elem.innerHTML = null;
@@ -99,7 +98,7 @@ function add_fields(elem_id){
     const fields = [];
 
     myApp.field_names.forEach(fieldname => {
-        let field = Object.create(field_factory());
+        let field = Object.create(FieldDelegator());
         field.setup(fieldname);
         field.elem.innerHTML = "<label class='lbl_class'>" + fieldname + "</label>";
         field.addTo(parent);
@@ -110,20 +109,17 @@ function add_fields(elem_id){
     return fields
 }
 
-function Btn_Factory(){
+function BtnDelegator(){
     const Button = Object.create(myApp.Widget);
   
     Button.setup = function(id){
-        this.init();
-        this.id = id;
-        this.elem = document.createElement("btn");
-        this.width = 20;
-        this.height = 20;    
+        this.init(id, document.createElement("btn"));
+        return this
     },
     Button.define = function(){
-        this.elem.innerText = null;
-        this.toggle = 0;
-        this.className = null;
+        this.elem.name = null;
+        this.elem.toggle = 0;
+        this.elem.className = null;
     },
     Button.builder = function(parent){
         this.addTo(parent);
@@ -131,14 +127,14 @@ function Btn_Factory(){
     return Button
 }
 
-function Image_Factory(){
+function ImageDelegator(){
     const Image = Object.create(myApp.Widget);
 
-    Image.setup = function(){
-        this.init();
-        this.elem = document.createElement("img");
+    Image.setup = function(id){
+        this.init(id, document.createElement("img"));
         this.elem.width = 20;
         this.elem.height = 20; 
+        return this
     },
     Image.define = function(){
         this.elem.src = "#";
@@ -152,9 +148,7 @@ function Image_Factory(){
 }
 
 function add_btns(fields){
-    console.log(fields)
     fields.forEach(field => {
-        //const parent = field.BtnPanel;
         let fragment = document.createDocumentFragment();
         fragment = add_btn(field, "Edit Label", "images/label.png", "Edit Label", fragment);
         fragment = add_btn(field, "Visible", "images/light_on.svg", "Visible", fragment);
@@ -163,16 +157,17 @@ function add_btns(fields){
         fragment = add_btn(field, "Digit", "images/decimal.png", "Digit", fragment);
         field.BtnPanel.appendChild(fragment);
     });
+    return fields
 }
 
 function add_btn(field, title, src, alt, fragment){
-    const btn = Object.create(Btn_Factory());
-    const img = Object.create(Image_Factory());
+    const btn = Object.create(BtnDelegator());
+    const img = Object.create(ImageDelegator());
 
     btn.setup(field.id);
-    btn.define();
+    btn.elem.className = "test"
 
-    img.setup();
+    img.setup(field.id);
     img.elem.title = title;
     img.elem.src = src;
     img.elem.alt = alt;
@@ -183,29 +178,6 @@ function add_btn(field, title, src, alt, fragment){
 }
 
 
-function Test_Factory(){
-    const Test = Object.create(field_factory());
-
-    Test.setup2 = function(){
-        this.init();
-        this.elem = document.createElement("img");
-    },
-    Test.define2 = function(){
-        this.test = null
-    },
-    Test.builder2 = function(parent){
-        this.addTo(parent)
-    }
-    return Test
-}
-
-function test(){
-    const test = Object.create(Test_Factory());
-
-    test.setup();
-    console.log(test.elem)
-
-}
 
 
 // ======================================================================
